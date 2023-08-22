c26da8d3fd5c4117400ecfcacc0630ac2383ee09
yellowyoshispeed <138832367+yellowyoshispeed@users.noreply.github.com>
2023-08-22 19:24:14 +0000

    I really hate this computer

--- src/main/java/u1pp/Main.java
+++ src/main/java/u1pp/Main.java
@@ -9,22 +9,25 @@ public class Main
         
         // Use this space to test your code, or actually run your project
         System.out.println("\n\nHi! This is an empty project.\nYour code will start running from Main.java.\nIt may be daunting to start a project from scratch, but try to have fun with it! :)\n");
+
+        
+        printGreeting("Noah");
     }
     
     /*
      * returns the average of 3 integers. 
      * Should return a double that is mathematically accurate. 
      */
-    public static double averageOfThree(int a, int b, int c) {
-    	return (a + b + c) / 3;
+    public static double averageOfThree(double a, double b, double c) {
+    	return (a + b + c) / 3.0;
     }
     
     /**
      * Prints a 1-line greeting based on the name given
      */
     public static void printGreeting(String name) {
-    	System.out.println("hello");
-    	System.out.println(name);
+    	System.out.print("Hello ");
+    	System.out.print(name + " ");
     }
     
 }
